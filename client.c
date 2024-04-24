a2f910aa14825b8b18b42431e058059e1a2f1635
tomnouk <anoukmournard@gmail.com.com>
2024-04-24 16:24:10 +0200

    ok

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: anomourn <anomourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 14:30:12 by anomourn          #+#    #+#             */
-/*   Updated: 2024/04/23 17:47:30 by anomourn         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:18:45 by anomourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	send_signal(int pid, unsigned int c)
 	while (i > 0)
 	{
 		i--;
-		c >>= 1;
 		if (c % 2 == 0)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
+		c >>= 1;
 		usleep(100);
 	}
 }
@@ -36,21 +36,19 @@ int		main(int argc, char **argv)
 	int	pid;
 
 	i = 0;
-	pid = ft_atoi(argv[1]);
-	if (argc == 3)
-	{
-		while (argv[2][i])
-			send_signal(pid, argv[2][i++]);
-	}
-	else if (pid <= 0)
-	{
-		ft_printf("PID invalid\n");
-		exit(EXIT_FAILURE);
-	}
-	else
+	if (argc != 3)
 	{
 		ft_printf("wrong number of arg\n");
 		exit(EXIT_FAILURE);
 	}
+	pid = ft_atoi(argv[1]);
+	//printf("PID : %i\n", pid);
+	if (pid <= 0)
+	{
+		ft_printf("PID invalid\n");
+		exit(EXIT_FAILURE);
+	}
+	while (argv[2][i])
+		send_signal(pid, argv[2][i++]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: anomourn <anomourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 14:30:06 by anomourn          #+#    #+#             */
-/*   Updated: 2024/04/23 15:51:26 by anomourn         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:19:56 by anomourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,41 @@
 
 void	handle_sig(int bit)
 {
-	static unsigned char c;
+	static char c;
 	static int len;
 
 	c <<= 1;
-	c |= bit == SIGUSR2;
+	c |= bit == SIGUSR1;
 	len++;
+	//ft_printf("%c", bit == SIGUSR1 ? '1' : '0');
 	if (len == 8)
 	{
 		ft_printf("%c", c);
 		if (c == 0)
 		{
-			if(kill(getppid(), SIGUSR2) == -1)
-				exit(EXIT_FAILURE);
+			// if(kill(getppid(), SIGUSR1) == -1)
+			// 	exit(EXIT_FAILURE);
 			ft_printf("\n");
 		}
-		else
-		{
-			if(kill(getppid(), SIGUSR1) == -1)
-				exit(EXIT_FAILURE);
-		}
+		// else
+		// {
+		// 	if(kill(getppid(), SIGUSR2) == -1)
+		// 		exit(EXIT_FAILURE);
+		// }
 		len = 0;
 	}
 }
 
 int	main(void)
 {
+	struct sigaction sig;
+	
+	sig = (struct sigaction){0};
+	sig.sa_handler = handle_sig;
+	sig.sa_flags = SA_SIGINFO;
 	ft_printf("PID : %i\n", getpid());
-	signal(SIGUSR1, handle_sig);
-	signal(SIGUSR2, handle_sig);
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
 	while (1)
 	{
 		pause();
